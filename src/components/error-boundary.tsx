f4265da691be5b31c4bb67a210ec2c3322c1373e
ByeongChan <thdqudcks2@gmail.com>
2024-07-09 17:10:58 +0900

    FEAT: error handling
    - 커스텀 에러 class 생성
    - axios 에러 구분
    - react-error-boundary 인증 에러 핸들링 기능 추가

--- src/components/error-boundary.tsx
+++ src/components/error-boundary.tsx
@@ -1,15 +1,28 @@
 "use client";
 
 import { QueryErrorResetBoundary } from "@tanstack/react-query";
+import { ErrorInfo } from "react";
 import { ErrorBoundary as ReactErrorBoundary } from "react-error-boundary";
 
 import { Button } from "@/components/ui/button";
+import { CustomError } from "@/lib/customError";
 
-function FallbackRender({ error, resetErrorBoundary }: { error: Error | null; resetErrorBoundary: () => void }) {
+// 통합 에러 핸들링 함수
+const handleError = (error: CustomError, info: ErrorInfo) => {
+  // 인증 오류 처리
+  if (error.statusCode === 401) {
+    alert(error.statusCode);
+    location.replace("/login");
+  }
+};
+
+// 핸들링 되지 못해서 throw 받은 에러
+function FallbackRender({ error, resetErrorBoundary }: { error: CustomError | null; resetErrorBoundary: () => void }) {
   console.error(error);
+
   return (
     <div role="alert" className="flex flex-col items-center justify-center">
-      <p className="mb-2 text-2xl font-bold">요청사항을 처리하는 중 오류가 발생했습니다.</p>
+      <p className="mb-2 text-2xl font-bold">{error?.message ?? "요청사항을 처리하는 중 오류가 발생했습니다."}</p>
       <p className="mb-4 font-medium">잠시 후 다시 시도해 주세요.</p>
       <Button onClick={resetErrorBoundary}>다시 시도</Button>
     </div>
@@ -18,7 +31,7 @@ function FallbackRender({ error, resetErrorBoundary }: { error: Error | null; re
 
 type ErrorBoundaryProps = {
   children: React.ReactNode;
-  FallbackComponent?: (props: { error: Error | null; resetErrorBoundary: () => void }) => React.ReactNode;
+  FallbackComponent?: (props: { error: CustomError | null; resetErrorBoundary: () => void }) => React.ReactNode;
 };
 
 export default function ErrorBoundary({ children, FallbackComponent }: ErrorBoundaryProps): JSX.Element {
@@ -27,6 +40,7 @@ export default function ErrorBoundary({ children, FallbackComponent }: ErrorBoun
       {({ reset }) => (
         <ReactErrorBoundary
           onReset={reset}
+          onError={handleError}
           FallbackComponent={(props) => (FallbackComponent ? FallbackComponent(props) : <FallbackRender {...props} />)}
         >
           {children}
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 import { ApiEndpoint, apiEndpoint } from "@/lib/api-dict";
+import { CustomError } from "@/lib/customError";
 
 const api = (function () {
   const instance = axios.create({
@@ -14,23 +15,46 @@ const api = (function () {
   return instance;
 })();
 
+const handleApiError = (error: unknown): never => {
+  if (axios.isAxiosError(error) && error.response) {
+    // 서버가 응답을 반환한 경우
+    switch (error.response.status) {
+      case 400:
+        throw new CustomError("Request에 문제가 있습니다.");
+      case 401:
+        throw new CustomError("Request에 문제가 있습니다.", 401);
+      case 500:
+        throw new CustomError("서버에 문제가 발생했습니다.");
+      default:
+        throw new CustomError("알 수 없는 에러가 발생했습니다.");
+    }
+  } else {
+    // 요청을 설정하는 동안 문제가 발생한 경우
+    throw new CustomError("요청 설정에 문제가 발생했습니다.");
+  }
+};
+
 const apiRequest = async <T_Key extends keyof ApiEndpoint>(
   key: T_Key,
   req: ApiEndpoint[T_Key]["req"],
 ): Promise<ApiEndpoint[T_Key]["res"]> => {
   const { url, method } = apiEndpoint[key];
 
-  const res = await api.request({
-    url,
-    method,
-    data: method !== "GET" ? req : undefined,
-    params: method === "GET" ? req : undefined,
-  });
+  try {
+    const res = await api.request({
+      url,
+      method,
+      data: method !== "GET" ? req : undefined,
+      params: method === "GET" ? req : undefined,
+    });
 
-  // TODO: BackEnd에서 요청 실패 시 응답하는 정의 필요
-  if (!res.data) throw new Error("데이터를 불러오지 못했습니다.");
+    if (!res.data) throw new CustomError("데이터를 불러오지 못했습니다.");
 
-  return res.data;
+    return res.data;
+  } catch (error) {
+    handleApiError(error);
+    throw Error;
+  }
 };
 
 export default apiRequest;
